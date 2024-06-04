7fa15090791c41663f1fad831bec77fb056d5f40
sidchopp <sidchopp@gmail.com>
2024-06-04 11:11:54 -0600

    Basic folders created

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { Hero } from "./hero/Hero";
+import { Technologies } from "./technologies/Technologies";
+import { Projects } from "./projects/Projects";
+import { Footer } from "./footer/Footer";
+
+export { Hero, Technologies, Projects, Footer };
