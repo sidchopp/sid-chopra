729dd78aa7818fed5358ba43e706f0efeefa6aaf
sidchopp <sidchopp@gmail.com>
2024-06-08 16:11:27 -0600

    removed extra padding, cards move on hover, minor css tweaks

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -14,7 +14,7 @@ const About = () => {
         <h2>Story</h2>
         <p>
           After completing my Master's degree, I started my career as a Physics
-          instructor, I cultivated a deep understanding of the universe's
+          instructor and cultivated a deep understanding of the universe's
           fundamental principles. Transitioning to software development, this
           passion for knowledge and innovation continues to drive me forward.
         </p>
@@ -23,8 +23,9 @@ const About = () => {
         <h2>Mission</h2>
         <p>
           I try to bridge the gap between science and technology, leveraging
-          insights from my education and experience cutting-edge solutions. Join
-          us on our journey of discovery and innovation.
+          insights from my education and experience to find cutting-edge
+          solutions to some of the world's real problems. Join me on my journey
+          of discovery and innovation.
         </p>
       </div>
     </Layout>
