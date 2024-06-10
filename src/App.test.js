b40369e7b2881bfe53ec22f33ec1ab74f9b38761
sidchopp <sidchopp@gmail.com>
2024-06-10 13:54:14 -0600

    Modified App tests for checking Routes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders SC or home page link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/chopra/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const scLink = screen.getByRole("link", { name: "SC" });
+
+  expect(scLink).toBeInTheDocument();
+  expect(scLink).toHaveAttribute("href", "/");
+});
+
+test("renders About page link", () => {
+  render(<App />);
+
+  const aboutLink = screen.getByRole("link", { name: "About" });
+
+  expect(aboutLink).toBeInTheDocument();
+  expect(aboutLink).toHaveAttribute("href", "/about");
+});
+
+test("renders Projects page link", () => {
+  render(<App />);
+
+  const projectsLink = screen.getByRole("link", { name: "Projects" });
+
+  expect(projectsLink).toBeInTheDocument();
+  expect(projectsLink).toHaveAttribute("href", "/projects");
+});
+
+test("renders Resume page link", () => {
+  render(<App />);
+
+  const resumeLink = screen.getByRole("link", { name: "My Resume" });
+
+  expect(resumeLink).toBeInTheDocument();
+  expect(resumeLink).toHaveAttribute("href", "/resume");
 });
