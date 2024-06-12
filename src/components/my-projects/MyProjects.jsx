d05f61ebd715876e7f3252dec8f4f79a1f189932
sidchopp <sidchopp@gmail.com>
2024-06-12 15:23:22 -0600

    Projects card on hover zooms in img

--- src/components/my-projects/MyProjects.jsx
+++ src/components/my-projects/MyProjects.jsx
@@ -15,13 +15,16 @@ const MyProjects = () => {
             key={project.id}
           >
             <article className={styles.article}>
-              <img
-                alt="placeholder"
-                className={styles.image}
-                height={263}
-                src={project.img}
-                width={350}
-              />
+              <span className={styles["image-container"]}>
+                <img
+                  alt="placeholder"
+                  className={styles.image}
+                  height={263}
+                  src={project.img}
+                  width={350}
+                />
+              </span>
+
               <div className={styles.content}>
                 <h3 className={styles.title}>{project.title}</h3>
                 <p className={styles.techUsed}>Tech: {project.techUsed}</p>
